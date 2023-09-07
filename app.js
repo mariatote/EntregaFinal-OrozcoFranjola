0ef9bd1a25ca7e41f8cc5cf4d12162fa635edd9f
Maria Jose Orozco Franjola <toteorozco@gmail.com>
2023-09-07 02:47:21 -0300

    se inteo hacer modal en roadmerch y se editaron las fotos de esa seccion

--- app.js
+++ app.js
@@ -1,26 +1,25 @@
-// Get the modal 2
-var modal2 = document.getElementById("myModal2");
+// Get the modal
+var modal = document.getElementById("myModal");
 
-// Get the button 2 that opens the modal
-var botonmodal2 = document.getElementById("botonmodal2");
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
 
-// When the user clicks the second button, open the second modal 
-botonmodal2.onclick = function() {
-modal2.style.display = "block";
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
-modal.style.display = "none";
-modal2.style.display = "none";
+  modal.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-if (event.target == modal) {
+  if (event.target == modal) {
     modal.style.display = "none";
-}
-if (event.target == modal2) {
-    modal2.style.display = "none";
-}
+  }
 }
